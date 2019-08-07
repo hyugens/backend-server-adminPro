6db71f4e3371af7cc2c68fe0bc6574ca21050a5b
Diego Molina <demm92@gmail.com>
2019-08-06 20:50:40 -0500

    Usuario y token listo

--- app.js
+++ app.js
@@ -1,19 +1,27 @@
 var express = require('express');
 var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
 
 var app = express();
+
+// Body-Parser
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+var appRoutes = require('./routes/app');
+var usuarioRoutes = require('./routes/usuario');
+var loginRoutes = require('./routes/login');
+
+
 
 mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB', (err, res) => {
     if (err) throw err;
     console.log('Base de datos: \x1b[32m%s\x1b[0m', 'online');
 })
 
-app.get('/', (req, res, next) => {
-    res.status(200).json({
-        ok: true,
-        mensaje: 'Peticion realizada correctamente'
-    });
-});
+app.use('/usuario', usuarioRoutes);
+app.use('/login', loginRoutes);
+app.use('/', appRoutes);
 
 app.listen(3000, () => {
     console.log('Listening on port 3000: \x1b[32m%s\x1b[0m', 'online');
